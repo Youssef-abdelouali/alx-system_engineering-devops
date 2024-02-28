381a4da2554b39979c0b606cac2970d9340adb19
Youssef-abdelouali <youssefabdelouali225@gmail.com>
2024-02-28 04:30:25 -0800

    regexp

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,2 +1,10 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hb?t?n/).join
+
+# Retrieve the first argument provided from the command line
+input = ARGV[0]
+
+# Check if the input matches the pattern
+match = input.match(/hbt{2,5}n/)
+
+# If a match is found, print it, otherwise print an empty string
+puts match ? match.to_s : ""
