2a1df9aa7d506a6b2a0e7a2aceccc0a599cc2871
Youssef-abdelouali <youssefabdelouali225@gmail.com>
2024-02-28 04:27:40 -0800

    regexp

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 # Retrieves the first argument from the command line.
-# Scans it for the pattern "hb?t?n" where '?' denotes zero or one occurrence of the preceding character.
+# Scans it for the pattern "hb?t?n" where '?' 
+# denotes zero or one occurrence of the preceding character.
 # Joins all the matching occurrences together and prints the result.
 puts ARGV[0].scan(/hb?t?n/).join
-
