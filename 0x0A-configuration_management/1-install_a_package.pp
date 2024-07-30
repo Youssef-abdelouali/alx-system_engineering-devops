0ae1725f464d271ee1802bdb7dc20d52f3dd8fe4
Youssef Abdelouali <youssefabdelouali255@gmail.com>
2024-07-30 07:42:48 -0700

    tasks done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-# This Puppet manifest installs the Flask package with version 2.1.0 using pip3
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin', '/usr/local/bin'],
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+# This Puppet manifest installs Flask version 2.1.0 using pip3 as the package provider.
+# The 'ensure' attribute specifies the exact version of Flask to install.
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => pip3,
 }
