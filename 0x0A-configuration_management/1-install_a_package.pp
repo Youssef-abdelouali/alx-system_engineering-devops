b7366c7bdb0f8607a32a3bc1da113831e405ffd7
Youssef Abdelouali <youssefabdelouali255@gmail.com>
2024-07-30 07:49:35 -0700

    taks done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# This Puppet manifest installs Flask version 2.1.0 using pip3 as the package provider.
-# The 'ensure' attribute specifies the exact version of Flask to install.
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => pip3,
+# Puppet manifest to install Flask and Werkzeug using pip3
+exec { 'install_flask_and_werkzeug':
+  command => 'pip3 install Flask==2.1.0 Werkzeug==2.1.1',
+  path    => ['/usr/local/bin', '/usr/bin', '/bin'],
+  unless  => 'pip3 show Flask | grep 2.1.0 && pip3 show Werkzeug | grep 2.1.1',
 }
