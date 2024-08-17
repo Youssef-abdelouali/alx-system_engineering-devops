8b0ca7783ba720f0559566254c9d6e363b9c3f66
Youssef Abdelouali <youssefabdelouali255@gmail.com>
2024-08-17 04:03:08 -0700

    debugging tasks

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Use Puppet manifest to resolve a wp-settings.php bug
+
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
